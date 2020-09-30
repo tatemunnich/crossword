826920519138e38d8f0a06b8ca71937e660dd983
Spencer McNall <spencermcnall@gmail.com>
2020-09-29 20:07:37 -0500

    Added content to panels. Changed menu.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,15 +7,21 @@ import Panel from "./Panel";
 class Menu extends React.Component {
     render() {
         return (
-            <div>
-                <button className={"menu-button"} value={"A"} onClick={this.props.onClick}>Help</button>
-                <button className={"menu-button"} value={"B"} onClick={this.props.onClick}>Words</button>
-                <button className={"menu-button"} value={"C"} onClick={this.props.onClick}>Export</button>
-                <button className={"menu-button"} value={"D"} onClick={this.props.onClick}>D</button>
-                <button className={"menu-button"} value={"E"} onClick={this.props.onClick}>E</button>
-                <button className={"menu-button"} value={"F"} onClick={this.props.onClick}>F</button>
-                <button className={"menu-button"} value={"G"} onClick={this.props.onClick}>G</button>
-                <button className={"menu-button"} value={"H"} onClick={this.props.onClick}>H</button>
+            <div className={"menubar"}>
+                <div className={"menubar-left"}>
+                    <button className={"menu-button"} value={"A"} onClick={this.props.onClick}>New</button>
+                    <button className={"menu-button"} value={"B"} onClick={this.props.onClick}>Help</button>
+                    <button className={"menu-button"} value={"C"} onClick={this.props.onClick}>Words</button>
+                    <button className={"menu-button"} value={"D"} onClick={this.props.onClick}>Clues</button>
+                    <button className={"menu-button"} value={"E"} onClick={this.props.onClick}>Export</button>
+                    {/*<button className={"menu-button"} value={"F"} onClick={this.props.onClick}>F</button>*/}
+                    {/*<button className={"menu-button"} value={"G"} onClick={this.props.onClick}>G</button>*/}
+                    {/*<button className={"menu-button"} value={"H"} onClick={this.props.onClick}>H</button>*/}
+                </div>
+                <div className={"menubar-right"}>
+                    <button className={"menu-button"}>Undo</button>
+                    <button className={"menu-button"}>Redo</button>
+                </div>
             </div>
         )
     }
