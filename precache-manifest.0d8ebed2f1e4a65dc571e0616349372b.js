86c0f150eb0bb8019af8f3897b7d01503ad28dcc
Tate Munnich <tatemunnich@gmail.com>
2020-09-29 22:29:12 -0500

    Updates

---
+++ precache-manifest.0d8ebed2f1e4a65dc571e0616349372b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffca37b0765dbabe2814ea50c3634561",
+    "url": "/crossword/index.html"
+  },
+  {
+    "revision": "3c5bc95ccfd17845956b",
+    "url": "/crossword/static/css/main.3334c550.chunk.css"
+  },
+  {
+    "revision": "c0f8e6b2d0cda0b30f0a",
+    "url": "/crossword/static/js/2.c135fffd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crossword/static/js/2.c135fffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5bc95ccfd17845956b",
+    "url": "/crossword/static/js/main.b8820f1a.chunk.js"
+  },
+  {
+    "revision": "e7212ad0492691aaf7c8",
+    "url": "/crossword/static/js/runtime-main.4c3f6f8d.js"
+  }
+]);
